2ab69b9994ea2d764ab7f5ca213d9c0cb3defdd2
Lucas Dupin <dupin@google.com>
2020-03-14 14:44:40 -0700

    raphael: Enable blurs, under dev-option
    Test: make
    Test: manual
    Bug: 149792636
    Change-Id: I3352b34739f5f45efa4088044de0e030449ff866

--- msmnile.mk
+++ msmnile.mk
@@ -186,6 +186,12 @@ PRODUCT_PRODUCT_PROPERTIES += \
     ro.surface_flinger.supports_background_blur=1 \
     ro.sf.blurs_are_expensive=1
 
+# Enable blurs, hidden under dev option
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.surface_flinger.supports_background_blur=1 \
+    persist.sys.sf.disable_blurs=1 \
+    ro.sf.blurs_are_expensive=1
+
 # Fingerprint
 PRODUCT_PACKAGES += \
     vendor.lineage.biometrics.fingerprint.inscreen@1.0-service.xiaomi_raphael
