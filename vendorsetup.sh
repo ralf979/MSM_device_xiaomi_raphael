e33bc9d74ef58407cadc7b51eb1c74d682feacd7
ralf979 <ralf979@gmail.com>
2020-12-08 22:54:35 +0100

    raphael: track hals & vibrator from vendorsetup

---
+++ vendorsetup.sh
@@ -0,0 +1,6 @@
+rm -rf hardware/qcom-caf/sm8150
+rm -rf vendor/qcom/opensource/vibrator
+git clone https://github.com/ralf979/vendor_qcom_opensource_vibrator vendor/qcom/opensource/vibrator
+git clone https://github.com/ralf979/hardware_qcom-caf_sm8150_audio hardware/qcom-caf/sm8150/audio
+git clone https://github.com/ralf979/hardware_qcom-caf_sm8150_display hardware/qcom-caf/sm8150/display
+git clone https://github.com/ralf979/hardware_qcom-caf_sm8150_media hardware/qcom-caf/sm8150/media
